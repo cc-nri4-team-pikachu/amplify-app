f6fd3b5c2c77c5bf9a608c768c0ad361c6b716fa
本田真暉 <hondamakotoakira@hondamakotoakiranoMacBook-Pro.local>
2023-11-24 15:22:45 +0900

    Change serverless-express to aws-serverless-express v1

--- amplify/backend/function/cardList/src/index.js
+++ amplify/backend/function/cardList/src/index.js
@@ -1,44 +1,15 @@
-const serverlessExpress = require('@vendia/serverless-express')
-//const serverlessExpress = require('aws-serverless-express')
+const awsServerlessExpress = require('aws-serverless-express');
 const app = require('./app');
 
 /**
  * @type {import('http').Server}
  */
-const server = serververlessExpress.createServer(app);
+const server = awsServerlessExpress.createServer(app);
 
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
 exports.handler = (event, context) => {
   console.log(`EVENT: ${JSON.stringify(event)}`);
-  return serverlessExpress.proxy(server, event, context);
+  return awsServerlessExpress.proxy(server, event, context, 'PROMISE').promise;
 };
-
-// /**
-//  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
-//  */
-// exports.handler = async (event, context) => {
-//   console.log(`EVENT: ${JSON.stringify(event)}`);
-//   context.callbackWaitsForEmptyEventLoop = false;
-//   return serververlessExpress( server ,event, context);
-// };
-
-// let serverlessExpressInstance
-
-// function asyncTask () {
-//   return new Promise((resolve) => {
-//     setTimeout(() => resolve('connected to database'), 1000)
-//   })
-// }
-
-// async function setup (event, context) {
-//   const asyncValue = await asyncTask()
-//   console.log(asyncValue)
-//   serverlessExpressInstance = serverlessExpress({ app })
-//   return serverlessExpressInstance(event, context)
-// }
-
-// exports.handler = (event, context) =>{
-//   // if (serverlessExpressInstance) return serverlessExpressInstance(event, context)
-
-//   // return setup(event, context)
-//   return serverlessExpress({ app })(event, context)
-// }
